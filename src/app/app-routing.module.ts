f97f789e5ac8654f84d91ff73b7cbcff0460384e
AlaaKammoun <alaa.kammoun@hotmail.com>
2019-01-20 16:08:02 +0200

    all source code

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes =
+[
+  {path : 'products' , component : ProductsComponent},
+  {path : 'orders' , component : MyOrdersComponent},
+  {path : 'admin/products' , component : AdminProductsComponent},
+  {path : 'admin/orders' , component : AdminOrdersComponent},
+  {path : 'login' , component : LoginComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    ProductsComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
